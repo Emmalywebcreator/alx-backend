9d4668760f92ec3449fefda84c2a190c94b18148
Emmanuel Ohwoka <emmyprime2015@gmail.cocm>
2024-10-28 17:55:50 +0000

    hyper

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -55,7 +55,9 @@ class Server:
             return []
         return dataset[start_index:end_index]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Optional[int]]:
+    def get_hyper(
+            self, page: int = 1, page_size: int = 10
+            ) -> Dict[str, Optional[int]]:
         """
         Get paginated data with additional hypermedia information.
         """
@@ -66,11 +68,11 @@ class Server:
         next_page = page + 1 if page < total_pages else None
         prev_page = page - 1 if page > 1 else None
 
-       return {
+        return {
             "page_size": len(data),
             "page": page,
             "data": data,
             "next_page": next_page,
             "prev_page": prev_page,
             "total_pages": total_pages
-        } 
+        }

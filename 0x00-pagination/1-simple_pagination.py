094f1e16ce48d6c8e4f8782149fc9d2f4c896af7
Emmanuel Ohwoka <emmyprime2015@gmail.cocm>
2024-10-28 16:31:24 +0000

    Get a dataset page

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -42,15 +42,15 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """A method that get a page from a dataset with pagination
-            and return it
-            """
-            assert isinstance(page, int) page > 0
-            assert isinstance(page_size, int) and page_size > 0
+        """A method that get a page from a dataset with pagination
+        and return it
+        """
+        assert isinstance(page, int) page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-            start_index, end_index = index_range(page, page_size)
+        start_index, end_index = index_range(page, page_size)
 
-            dataset = self.dataset()
-            if start_index >= len(dataset):
-                return []
-            return dataset[start_index:end_index]
+        dataset = self.dataset()
+        if start_index >= len(dataset):
+            return []
+        return dataset[start_index:end_index]

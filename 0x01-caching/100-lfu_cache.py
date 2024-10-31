00be9fa672b6e4c2cd0f63ac773f9eb88ed6c223
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-10-31 06:10:16 +0100

    lfu

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """"
-A caching system that discards the least frequently used item when the 
-cache exceeds the limit. If items have the same frequency, it discards 
-the least recently used item among 
+A caching system that discards the least frequently used item when the
+cache exceeds the limit. If items have the same frequency, it discards
+the least recently used item among
 """
 
 from base_caching import BaseCaching
@@ -13,15 +13,16 @@ class LFUCache(BaseCaching):
     """
     List frequently used
     """
-    
+
     def __init__(self):
         """Initialize the cache system and additional tracking structures."""
         super().__init__()
         self.frequency = defaultdict(int)
         self.recency = OrderedDict()
-        
+
     def put(self, key, item):
-        """Add an item to the cache, and apply LFU + LRU replacement policy when limit exceeded."""
+        """Add an item to the cache, and apply LFU + LRU replacement policy when
+        limit exceeded."""
         if key is None or item is None:
             return
         if key in self.cache_data:
@@ -30,13 +31,16 @@ class LFUCache(BaseCaching):
             return
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             self._discard_lfu_item()
-            
+
         self.cache_data[key] = item
         self.frequency[key] = 1
         self._update_recency(key)
-        
+
     def get(self, key):
-        """Retrieve an item from the cache by key, updating frequency and recency."""
+        """
+        Retrieve an item from the cache by key, updating frequency
+        and recency.
+        """
         if key is None or key not in self.cache_data:
             return None
         self._update_frequency_and_recency(key)
@@ -54,10 +58,15 @@ class LFUCache(BaseCaching):
         self.recency[key] = True
 
     def _discard_lfu_item(self):
-        """Remove the least frequently used (and least recently used in case of tie) item."""
+        """
+        Remove the least frequently used (and least recently used in case
+        of tie) item.
+        """
         min_freq = min(self.frequency.values())
-    
-        lfu_keys = [k for k, freq in self.frequency.items() if freq == min_freq]
+
+        lfu_keys = [
+            k for k, freq in self.frequency.items() if freq == min_freq
+        ]
 
         if len(lfu_keys) > 1:
             oldest_lfu_key = next(k for k in self.recency if k in lfu_keys)
@@ -67,5 +76,5 @@ class LFUCache(BaseCaching):
         del self.cache_data[oldest_lfu_key]
         del self.frequency[oldest_lfu_key]
         self.recency.pop(oldest_lfu_key)
-        
+
         print(f"DISCARD: {oldest_lfu_key}")

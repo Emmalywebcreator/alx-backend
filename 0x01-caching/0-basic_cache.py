215d5343fd0ca3833971ba780717c0450bb10f88
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-10-31 04:21:31 +0100

    Class fifo that remaoves the first item that enters a cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,4 +11,6 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """Retrieve an item from the cache by key."""
-        return self.cache_data.get(key, None)
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None

583cdf12855996c2298e49b0587ad4afe34fcf6c
Emmanuel Ohwoka <emmyprime2015@gmail.cocm>
2024-11-02 05:25:15 +0000

    mru:pycodestyle

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,20 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ MRU Cache Module """
 
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ 
+    """
     MRUCache defines a caching system following the Most Recently Used (MRU)
-    eviction policy. When the cache reaches its limit, it discards the most
-    recently used item to make room for new entries.
+    eviction policy.
     """
 
     def __init__(self):
         """
-        Initialize the MRUCache by calling the parent class initializer
-        and setting up an attribute to keep track of the most recently used key.
+        Initialize the MRUCache by calling the parent class
+        initializer and setting up an attribute to keep
+        track of the most recently used key.
         """
         super().__init__()
         self.most_recently_used_key = None
@@ -39,18 +39,8 @@ class MRUCache(BaseCaching):
     def get(self, key):
         """
         Retrieve an item from the cache by its key.
-        
-        Args:
-            key (str): The key associated with the item to be retrieved.
-
-        Returns:
-            The item associated with the key if it exists in the cache, 
-            or None if the key is not found or is None.
-
-        Accessing an item updates it as the most recently used item.
         """
         if key in self.cache_data:
             self.most_recently_used_key = key
             return self.cache_data[key]
         return None
-
